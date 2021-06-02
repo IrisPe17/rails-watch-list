ce2440283a33a90080e1682667c5005254e1eafb
Iris Perisa <iris.perisa@gmail.com>
2021-06-03 01:57:21 +0200

    seed update, depend. added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,10 @@
 require 'open-uri'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Bookmark.destroy_all
 Movie.destroy_all
 List.destroy_all
 
-url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
+url = 'http://tmdb.lewagon.com/movie/top_rated'
 
 response = URI.open(url)
 json = JSON.parse(response.read)
