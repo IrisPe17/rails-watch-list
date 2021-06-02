3adfe448bda7de39a4552d89a31a42542b0af0c2
Iris Perisa <iris.perisa@gmail.com>
2021-06-02 03:47:10 +0200

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
+
+response = URI.open(url)
+json = JSON.parse(response.read)
+
+movies = json['results']
+
+movies.each do |movie|
+  title = movie['title']
+  overview = movie['overview']
+  poster_url = movie['poster_path']
+  rating = movie['vote_average']
+  movie = Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  puts movie.title
+end
